d4a5f95d7cab0a7c844e5acb500eedf3bbcaeb90
Andy Gray <codingwithandy@gmail.com>
2020-01-27 08:58:19 +0000

    Add on to previous commit.

--- GHFollowers/GHFollowers/Custom Views/GFAvatarImageView.swift
+++ GHFollowers/GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
     
+    let cache = NetworkManager.shared.cache
     let placeholderImage = UIImage(named: "avatar-placeholder")
 
     
@@ -22,7 +23,9 @@ class GFAvatarImageView: UIImageView {
     
     
     required init?(coder: NSCoder) {
+    
         fatalError("init(coder:) has not been implemented")
+    
     }
     
     
@@ -35,5 +38,37 @@ class GFAvatarImageView: UIImageView {
         
     }
     
+    
+    func downloadImage(from urlString: String) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        // check if the image is not in cache
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = image
+            return
+        }
+        
+        // Getting the avitar image
+        guard let url = URL(string:  urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            self.cache.setObject(image, forKey: cacheKey)
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        
+        task.resume()
+        
+    }
 
 }
--- GHFollowers/GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/GHFollowers/Screens/FollowerListVC.swift
@@ -16,6 +16,8 @@ class FollowerListVC: UIViewController {
     
     var username: String!
     var followers: [Follower] = []
+    var page = 1
+    var hasMoreFollowers = true
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -25,7 +27,7 @@ class FollowerListVC: UIViewController {
         
         configureCollectionView()
         configureViewController()
-        getFollowers()
+        getFollowers(username: username, page: page)
         configureDataSource()
         
     }
@@ -46,13 +48,17 @@ class FollowerListVC: UIViewController {
     }
     
     
-    func getFollowers() {
+    func getFollowers(username: String, page: Int) {
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
+            guard let self = self else { return }
+            
             
             switch result {
                 case .success(let followers):
-                    self.followers = followers
+                    if followers.count < 100 { self.hasMoreFollowers = false }
+                    
+                    self.followers.append(contentsOf: followers)
                     self.updateData()
                 
                 case .failure(let error):
@@ -65,30 +71,16 @@ class FollowerListVC: UIViewController {
     
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
+        
+        collectionView.delegate = self
         
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
     }
     
-    
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
-    }
     
     func configureDataSource () {
         
@@ -110,8 +102,25 @@ class FollowerListVC: UIViewController {
         snapshot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
         
-        
     }
     
+}
+
+
+extension FollowerListVC: UICollectionViewDelegate {
     
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            guard hasMoreFollowers else { return }
+            page += 1
+            getFollowers(username: username, page: page)
+        }
+        
+    }
 }
